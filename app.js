2da13d87b0c2aa683a62619a71f570a57074aa72
baconeggsssss-lang <bacon.eggsssss@gmail.com>
2026-08-12 12:04:26 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -171,7 +171,7 @@ let audioActivationToken = 0;
 const LOUD_CHANNEL_VOLUME_OVERRIDES = {
   XWq5kBlakcQ: 70, // CNA
   vfszY1JYbMc: 50, // WION
-  "-JeNrQVjWQU": 70, // CCTV13
+  "-JeNrQVjWQU": 50, // CCTV13
   I4AVxPm58S0: 70, // NTN24
 };
 const TIME_FORMATTERS = {};
